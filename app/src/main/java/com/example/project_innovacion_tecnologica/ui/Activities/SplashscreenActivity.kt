9e05f69ffe58a8c1e0cb5405df87960fd893344c
Nicolas <nicolasgiovanny@hotmail.com>
2020-04-25 17:14:07 -0500

    Primera version del proyecto, se crean solo vistas

---
+++ app/src/main/java/com/example/project_innovacion_tecnologica/ui/Activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.project_innovacion_tecnologica.ui.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project_innovacion_tecnologica.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
